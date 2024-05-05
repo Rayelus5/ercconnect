9d879a277ad4e72dfcc5a53ac166bc7ec2cc832f
Rayelus <rpame876@gmail.com>
2024-05-05 21:47:08 +0200

    Some new files and fixed content

---
+++ ercteam/js/main.js
@@ -0,0 +1,22 @@
+let daoETH = document.querySelector("#daoETH");
+let daoUSD = document.querySelector("#daoUSD");
+let daoADMIN1 = document.querySelector("#daoADMIN1");
+
+
+
+//PRECIO ETH - ACTUALIZAR
+let ethPrice = 3150;
+
+//CANTIDAD ETH - ACTUALIZAR
+daoETH = 0.1045;
+
+
+
+daoUSD = Math.round(daoETH*ethPrice);
+daoADMIN1 = Math.round(daoUSD/3);
+
+document.querySelector("#daoETH").innerHTML = daoETH;
+document.querySelector("#daoUSD").innerHTML = daoUSD;
+document.querySelector("#daoADMIN1").innerHTML = daoADMIN1;
+document.querySelector("#daoADMIN2").innerHTML = daoADMIN1;
+document.querySelector("#daoADMIN3").innerHTML = daoADMIN1;
